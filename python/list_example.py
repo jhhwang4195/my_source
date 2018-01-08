31d3769df94cb90a1821a8b488a390f4e55e3db8
jhhwang <jhhwang4195@gmail.com>
2018-01-08 10:42:47 +0900

    Add list_example.py

---
+++ python/list_example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# tuple
+print ("##### tuple test")
+test_tuple = ('one', 'two', 'three')
+for i in test_tuple: 
+	print(i)
+
+# list
+print ("\n#####list test")
+test_list = ['one', 'two', 'three'] 
+for i in test_list: 
+	print(i)
