f9aef0fd800f89edf71b6fa92bd4fb0c13759c97
jhhwang <jhhwang4195@gmail.com>
2018-01-08 20:24:20 +0900

    remove white space

--- python/color.py
+++ python/color.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 #Reference: https://stackoverflow.com/questions/287871/print-in-terminal-with-colors
-def printr(prt): print("\033[91m {}\033[00m" .format(prt))
-def printg(prt): print("\033[92m {}\033[00m" .format(prt))
-def printy(prt): print("\033[93m {}\033[00m" .format(prt))
-def printb(prt): print("\033[94m {}\033[00m" .format(prt))
-def printt(prt): print("\033[30;47m {}\033[00m" .format(prt))
+def printr(prt): print("\033[91m{}\033[00m" .format(prt))
+def printg(prt): print("\033[92m{}\033[00m" .format(prt))
+def printy(prt): print("\033[93m{}\033[00m" .format(prt))
+def printb(prt): print("\033[94m{}\033[00m" .format(prt))
+def printt(prt): print("\033[30;47m{}\033[00m" .format(prt))
 
 printr("Hello World!!")
 printg("Hello World!!")
