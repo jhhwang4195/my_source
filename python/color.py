a49192ed3b1f9e57fad79ffa6d5a55dc7eb90c0e
jhhwang <jhhwang4195@gmail.com>
2018-01-08 20:09:41 +0900

    Add color.py

---
+++ python/color.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#Reference: https://stackoverflow.com/questions/287871/print-in-terminal-with-colors
+def printr(prt): print("\033[91m {}\033[00m" .format(prt))
+def printg(prt): print("\033[92m {}\033[00m" .format(prt))
+def printy(prt): print("\033[93m {}\033[00m" .format(prt))
+def printb(prt): print("\033[94m {}\033[00m" .format(prt))
+
+printr("Hello World!!")
+printg("Hello World!!")
+printb("Hello World!!")
+printy("Hello World!!")
