68677dcee4b407841a1da220f36930f1d2584f18
jhhwang <jhhwang4195@gmail.com>
2018-01-08 20:35:50 +0900

    Add color log

--- python/github_check.py
+++ python/github_check.py
@@ -12,6 +12,13 @@ import getopt
 local_tz = pytz.timezone('Asia/Seoul')
 
 
+def printr(prt): print("\033[91m{}\033[00m" .format(prt))
+def printg(prt): print("\033[92m{}\033[00m" .format(prt))
+def printy(prt): print("\033[93m{}\033[00m" .format(prt))
+def printb(prt): print("\033[94m{}\033[00m" .format(prt))
+def printt(prt): print("\033[30;47m{}\033[00m" .format(prt))
+
+
 def init_github3(user, password):
     gh = github3.login(user, password)
     return gh
@@ -21,12 +28,14 @@ def get_repo_last_commit_delta_time(gh, user, repo):
     try:
         repo = gh.repository(user, repo)
         if repo is None:
-            print ("[Error]: Not Found repository(user=%s, repoistory=%s)" % (user, repo))
+            printr("[Error]: Not Found repository(user=%s, repoistory=%s)" % (user, repo))
             sys.exit(1)
 
         return repo.pushed_at.astimezone(local_tz)
     except Exception as err:
-        print ("[Error] %s" % (str(err)))
+        printr("[Error] %s" % (str(err)))
+        printr("[Error] Check github user and password!!")
+        sys.exit(1)
 
 
 def get_delta_time(last_commit):
@@ -37,12 +46,12 @@ def get_delta_time(last_commit):
 
 def get_repository_info(gh, user, repo):
     repo = gh.repository(user, repo)
-    print "### user:%s, repository:%s" % (user, repo)
-    print "name: %s" % repo.name
-    print "owner: %s" % repo.owner
-    print "description: %s" % repo.description
-    print "created_at: %s" % repo.created_at
-    print "pushed_at: %s" % repo.pushed_at.astimezone(local_tz)
+    printg("### user:%s, repository:%s" % (user, repo))
+    printg("name: %s" % repo.name)
+    printg("owner: %s" % repo.owner)
+    printg("description: %s" % repo.description)
+    printg("created_at: %s" % repo.created_at)
+    printg("pushed_at: %s" % repo.pushed_at.astimezone(local_tz))
 
 
 def get_repository_commit(gh, user, repo):
@@ -50,9 +59,9 @@ def get_repository_commit(gh, user, repo):
     delta_time = get_delta_time(last_commit)
 
     if(delta_time == 0):
-        print ("%s/%s commited today." % (user, repo))
+        printg("%s/%s commited today." % (user, repo))
     else:
-        print ("%s/%s has not committed for %s days." % (user, repo, delta_time))
+        printg("%s/%s has not committed for %s days." % (user, repo, delta_time))
 
 
 def get_all_repository_commit(gh, user):
@@ -62,20 +71,22 @@ def get_all_repository_commit(gh, user):
 
 
 def usage():
-    print ("================================================================================")
-    print ("* User Input: %s" % sys.argv[1:])
-    print ("================================================================================")
-    print ("%s [u:p:r:ah]\n" % sys.argv[0])
-    print ("-u, --user        Set github username")
-    print ("-p, --password    Set github password")
-    print ("-r, --repo        Set github repository")
-    print ("-a, --all         Set all github repository")
-    print ("-h, --help        Print usage")
+    print("----------------------------------------------------------------------------")
+    print("* User Input: %s" % ' '.join(sys.argv[1:]))
+    print("----------------------------------------------------------------------------")
+    printt("  %s [u:p:r:ah]       \t\t\t\t\t    " % sys.argv[0])
+    print("----------------------------------------------------------------------------")
+    print("")
+    print("  -u, --user        Set github username")
+    print("  -p, --password    Set github password")
+    print("  -r, --repo        Set github repository")
+    print("  -a, --all         Set all github repository")
+    print("  -h, --help        Print usage")
 
-    print ("\n")
-    print ("* ex) python github_test.py -u jhhwang4195 -p password -r TIL,my_source")
-    print ("* ex) python github_test.py -u jhhwang4195 -p password -a")
-    print ("================================================================================")
+    print("\n")
+    printr("* ex) python github_test.py -u jhhwang4195 -p password -r TIL,my_source")
+    printr("* ex) python github_test.py -u jhhwang4195 -p password -a")
+    print("----------------------------------------------------------------------------")
 
 if __name__ == '__main__':
 
@@ -90,7 +101,7 @@ if __name__ == '__main__':
                                    ["user=", "password=", "repo=", "all=", "help"])
 
     except getopt.GetoptError, err:
-        print(err)
+        printr(err)
         sys.exit(-1)
 
     for o, val in opts:
@@ -111,23 +122,24 @@ if __name__ == '__main__':
             sys.exit(-1)
 
     if user is None:
-        print ("[Error] -u option is None")
+        printr("[Error] -u option is None")
         sys.exit(-1)
     elif password is None:
-        print ("[Error] -p option is None")
+        printr("[Error] -p option is None")
         sys.exit(-1)
     elif repos is None and all_repo is None:
-        print ("[Error] -r option or -a option is None")
+        printr("[Error] -r option or -a option is None")
+        usage()
         sys.exit(-1)
 
     gh = init_github3(user, password)
 
     if repos is not None:
-        print (">>> Selected repository info")
+        printr(">>> Selected repository info")
         repo = repos.split(",")
         for r in repo:
             get_repository_commit(gh, user, r)
 
     if all_repo is not None:
-        print (">>> All repository info")
+        printr(">>> All repository info")
         get_all_repository_commit(gh, user)
