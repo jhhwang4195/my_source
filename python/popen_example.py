4fd2a6a67a01a890d413437b431e7500e58875d3
jhhwang <jhhwang4195@gmail.com>
2018-01-09 15:14:51 +0900

    Update popen_example.py result

--- python/popen_example.py
+++ python/popen_example.py
@@ -8,3 +8,22 @@ print(p.read())
 
 vin, out, err = os.popen3('ls -la')
 print(out.read())
+
+"""
+########################################
+# Result
+########################################
+total 76
+drwxr-xr-x 7 root root 4096 Jan  9 15:08 .
+drwxr-xr-x 4 root root 4096 Jan  8 21:56 ..
+-rw-r--r-- 1 root root  118 Jan  8 21:56 basic.py
+-rw-r--r-- 1 root root  541 Jan  8 21:56 color.py
+-rw-r--r-- 1 root root  883 Jan  9 15:05 elapsed_time.py
+
+total 76
+drwxr-xr-x 7 root root 4096 Jan  9 15:08 .
+drwxr-xr-x 4 root root 4096 Jan  8 21:56 ..
+-rw-r--r-- 1 root root  118 Jan  8 21:56 basic.py
+-rw-r--r-- 1 root root  541 Jan  8 21:56 color.py
+-rw-r--r-- 1 root root  883 Jan  9 15:05 elapsed_time.py
+"""
