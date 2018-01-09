e97dc950a3e457cc87aac9541f1151fa22784316
jhhwang <jhhwang4195@gmail.com>
2018-01-09 15:22:52 +0900

    Update try_except_example.py result

--- python/try_except_example.py
+++ python/try_except_example.py
@@ -37,3 +37,42 @@ def test_file_open_except():
 if __name__ == '__main__':
     test_envrion_except()
     test_file_open_except()
+
+
+"""
+########################################
+# Result
+########################################
+[Error] 'GITHUB_USERNAME_TEST'
+>>>>>>>>>>>>>>> traceback.print_exc
+Traceback (most recent call last):
+File "try_except_example.py", line 9, in test_envrion_except
+var = os.environ['GITHUB_USERNAME_TEST']
+File "/usr/lib/python2.7/UserDict.py", line 23, in __getitem__
+raise KeyError(key)
+KeyError: 'GITHUB_USERNAME_TEST'
+None
+
+>>>>>>>>>>>>>>> traceback.format_exc
+Traceback (most recent call last):
+File "try_except_example.py", line 9, in test_envrion_except
+var = os.environ['GITHUB_USERNAME_TEST']
+File "/usr/lib/python2.7/UserDict.py", line 23, in __getitem__
+raise KeyError(key)
+KeyError: 'GITHUB_USERNAME_TEST'
+
+>>>>>>>>>>>>>>> traceback.print_stack
+File "try_except_example.py", line 38, in <module>
+test_envrion_except()
+File "try_except_example.py", line 18, in test_envrion_except
+print (traceback.print_stack())
+None
+
+[Error] [Errno 2] No such file or directory: 'test.txt'
+>>>>>>>>>>>>>>> traceback.print_exc
+Traceback (most recent call last):
+File "try_except_example.py", line 24, in test_file_open_except
+fp = open("test.txt", "r")
+IOError: [Errno 2] No such file or directory: 'test.txt'
+None
+"""
