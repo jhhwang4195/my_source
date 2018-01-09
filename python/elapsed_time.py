6adf9b88066267c385cc2e5eac39d9098a424e00
jhhwang <jhhwang4195@gmail.com>
2018-01-09 15:05:57 +0900

    Update result

--- python/elapsed_time.py
+++ python/elapsed_time.py
@@ -23,3 +23,14 @@ stop_time = datetime.now()
 print("My Stop Time: %s" % stop_time)
 elapsed_time = stop_time - start_time
 print("My Elapsed Time: %s" % elapsed_time)
+
+"""
+########################################
+# Result
+########################################
+elapsed_time: 2.22106719017
+Time elapsed (hh:mm:ss.ms): 0:00:02.222293
+My Start Time: 2018-01-09 15:05:13.598409
+My Stop Time: 2018-01-09 15:05:15.820810
+My Elapsed Time: 0:00:02.222401
+"""
