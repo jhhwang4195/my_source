ed6e064bfbce552791785bbbe180cf555c2394cf
jhhwang <jhhwang4195@gmail.com>
2018-01-09 09:05:41 +0900

    Update indent

--- python/signal/signal_signal.py
+++ python/signal/signal_signal.py
@@ -10,10 +10,10 @@ import time
 def receive_signal(signum, stack):
     print 'Received:', signum
 
-    signal.signal(signal.SIGUSR1, receive_signal)
-    signal.signal(signal.SIGUSR2, receive_signal)
+signal.signal(signal.SIGUSR1, receive_signal)
+signal.signal(signal.SIGUSR2, receive_signal)
 
-    print 'My PID is:', os.getpid()
+print 'My PID is:', os.getpid()
 
 while True:
     print 'Waiting...'
