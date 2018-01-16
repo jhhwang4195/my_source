ab75c7df8875c0d7359ce5b14f1f441c77698c3c
jhhwang <jhhwang4195@gmail.com>
2018-01-16 09:29:47 +0900

    Update log2.py

--- python/logging/log2.py
+++ python/logging/log2.py
@@ -38,6 +38,7 @@ if __name__ == '__main__':
     mylogger.info("server start!!!")
 """
 
+"""
 import logging
 
 if __name__ == '__main__':
@@ -54,3 +55,30 @@ if __name__ == '__main__':
     mylogger.addHandler(file_handler)
 
     mylogger.info("server start!!!")
+"""
+
+import logging
+import threading
+import time
+
+def worker(arg):
+    while not arg['stop']:
+        logging.debug('Hi from myfunc')
+        time.sleep(0.5)
+
+def main():
+    logging.basicConfig(level=logging.DEBUG, format='%(relativeCreated)6d %(threadName)s %(message)s')
+    info = {'stop': False}
+    thread = threading.Thread(target=worker, args=(info,))
+    thread.start()
+    while True:
+        try:
+            logging.debug('Hello from main')
+            time.sleep(0.75)
+        except KeyboardInterrupt:
+            info['stop'] = True
+            break
+    thread.join()
+
+if __name__ == '__main__':
+    main()
